437a8700f3852eac529a2c1df8aadbc1735a2e77
AlejandroAlonsoFigueroa <alejandro.figueroa.contractor@openpay.mx>
2023-12-14 17:13:00 -0600

    Agrega maqueta para editar empleado

--- php-apache/src/empleados.php
+++ php-apache/src/empleados.php
@@ -132,5 +132,40 @@
                 <?php }?> 
             </table>
         </div>
+
+        <!-- Formulario para actualizar, escondido -->
+        <div id ="contenedor-absoluto" style = "display: block; position: absolute; top: 0; left: 0; background-color: grey; width: 100%; height: 100%;">
+           <div style = "background-color: white; width: fit-content; padding: 50px; margin: 0 auto;">
+                    <form action = "guardarEmpleado.php" method = "POST">
+                        <div>
+                            <div>
+                                <div><p>Clave</p></div>
+                                <div><input type = "number" name = "clave" required /></div>
+                            </div>
+                            <div>
+                            <div><p>Nombre</p></div>
+                                <div><input type = "text" name = "nombre"required/></div>
+                            </div>
+                            <div>
+                                <div><p>Dirección</p></div>
+                                <div><input type = "text" name = "direccion" required/></div>
+                            </div>
+                            <div>
+                                <div><p>Teléfono</p></div>
+                                <div><input type = "text" name = "telefono" required/></div>
+                            </div>
+                        </div>
+                        <div style = "margin-top: 10px;">
+                            <button type = "submit">Editar empleado</button>
+                        </div>
+                    </form>
+           </div> 
+        </div>
+        <script>
+            function mostrarFormActualizar(){
+
+            }
+        </script>
+                
     </body>
 </html>
