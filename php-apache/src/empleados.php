f75cb2d80220c9b8cdefbdc3d20f32767b680ba9
AlejandroAlonsoFigueroa <alejandro.figueroa.contractor@openpay.mx>
2023-12-14 15:51:51 -0600

    Agrega función listar y mostrar empleados

--- php-apache/src/empleados.php
+++ php-apache/src/empleados.php
@@ -1,4 +1,11 @@
 <html>
+    <head>
+        <style>
+            table, tr, td{
+                border: solid black 1px;
+            }
+        </style>
+    </head>
     <?php 
          session_start();
         /*if(isset($_SESSION['id_usuario'])){
@@ -26,12 +33,25 @@
         }
 
         function obtenerEmpleados(){
+            $sql = "SELECT clave, nombre, direccion, telefono FROM empleado";
+            $conexion =  obtenerConexionBBDD();
+            $stmt = $conexion->prepare($sql);
+            $stmt->execute();
 
+            /*while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo  $row['clave'];
+                echo  $row['nombre'];
+                echo  $row['direccion'];
+            }*/
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
         function eliminarEmpleado(){
-            
+
         }
+
+
     ?>
+
     <head></head>
 
     <body>
@@ -39,11 +59,19 @@
         <h1>Administrar a los empleados</h1>
 
         <div>
-
+            <h2>Lista de empleados</h2>
         </div>
         <div>
-            <?php ?>
-            <h2>Lista de empleados</h2>
+            <table>
+                <?php foreach(obtenerEmpleados() as $row){ ?>
+                    <tr>
+                        <td><?php echo $row['clave']; ?></td>
+                        <td><?php echo $row['nombre']; ?></td>
+                        <td><?php echo $row['direccion']; ?></td>
+                        <td><?php echo $row['telefono']; ?></td>
+                    </tr>
+                <?php }?> 
+            </table>
         </div>
     </body>
 </html>
